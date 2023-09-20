3a025509510bf7d2de9cad933c0af9bdf90df6e9
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-09-19 23:58:48 -0400

    Proyecto base front

---
+++ front/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Listar from './components/listar/Listar'
+import Subir from './components/subir/Subir'
+
+function App() {
+
+  return (
+    <div className='container'>
+      <Listar />
+      <Subir />
+    </div>
+  )
+}
+
+export default App
