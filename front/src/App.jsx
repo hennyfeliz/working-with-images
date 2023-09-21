353dac3d08b7605a2ffd204c1225df3927d3c253
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-09-21 02:27:23 -0400

    Adecuando componente principal para integracion de react-router-dom y los componentes a renderizar con sus paths

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css'
 import Listar from './components/listar/Listar'
 import Subir from './components/subir/Subir'
+import { Route, Routes, BrowserRouter, Link } from 'react-router-dom'
 
 function App() {
 
   return (
-    <div className='container'>
-      <Listar />
-      <Subir />
-    </div>
+    <BrowserRouter>
+      <div className='container'>
+        <div className='up-buttons'>
+          <Link to='/'>
+            <button>Listar</button>
+          </Link>
+          <span>|</span>
+          <Link to='/subir'>
+            <button>Subir</button>
+          </Link>
+        </div>
+        <Routes>
+          <Route path='/' element={<Listar />} />
+          <Route path='/subir' element={<Subir />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
