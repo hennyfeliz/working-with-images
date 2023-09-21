6efaed92104c898324bdea857dded2e7489d8e49
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-09-21 02:26:14 -0400

    Icono trash para borrar imagenes y componente para subir imagenes en lista y estilos

--- front/src/components/subir/Subir.jsx
+++ front/src/components/subir/Subir.jsx
@@ -1,10 +1,40 @@
+import { useState } from 'react';
 import './Subir.css'
 
-
-
 const Subir = () => {
+
+  const [fetchResponse, setFetchResponse] = useState(false);
+
+  const uploadImages = (event) => {
+    const url = 'http://localhost:8080/many';
+    const files = event.target.files;
+    const formData = new FormData();
+
+    for (let i = 0; i < files.length; i++) {
+      formData.append('archivo', files[i]);
+    }
+
+    fetch(url, {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => response.text())
+      .then(data => {
+        setFetchResponse(data);
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error occurred while uploading images:', error);
+        throw error;
+      });
+  };
+
   return (
-    <div>Subir</div>
+    <div className='subir'>
+      <input type='file' onChange={uploadImages} multiple />
+      {/* <button onClick={uploadImages}>Enviar imagenes</button> */}
+      {fetchResponse ? <span>Carga completada...</span> : ''}
+    </div>
   )
 }
 
